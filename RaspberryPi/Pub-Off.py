b06f8217fcddd4be53b783fded9c82548d32649b
nik <140684917+Nik-RR@users.noreply.github.com>
2023-08-31 13:13:43 +1000

    Pub-Off.py

--- RaspberryPi/Pub-Off.py
+++ RaspberryPi/Pub-Off.py
@@ -1,4 +1,4 @@
 import paho.mqtt.publish as publish
 
-publish.single("ifn649","LED_OFF",hostname="3.27.156.48")
+publish.single("ifn649","LED_OFF",hostname="13.239.37.158")
 print("Done, LED is OFF")
